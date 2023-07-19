f391e3ca18bcdf0070333ec1fb5aca2bb488fd8d
noreyb <disce_uno@yahoo.co.jp>
2023-07-19 17:10:26 +0900

    add: <subject: what>
    <body: why>
    
    ---
    add: (機能・ファイルを)追加する
    fix: (コードを)修正する
    clean: (コードを)改善する; 名前の変更/コードの整形
    update: (パッケージやドキュメントなどを更新する)
    remove: (ファイルやコードを)除去する
    rename: (ファイル名を)変更する
    change: (AをBに)変更する
    
    ref: https://www.tam-tam.co.jp/tipsnote/program/post16686.html

--- sync_raindrops.py
+++ sync_raindrops.py
@@ -6,21 +6,10 @@ import os
 from urllib.parse import urlparse
 import pykakasi
 import re
+import sys
 
 
-if __name__ == "__main__":
-    with open("weneedfeed.yml", "r") as f:
-        feeds = yaml.safe_load(f)
-    print(feeds["pages"][0])
-
-    # Unify domains
-    for feed in feeds["pages"]:
-        feed["url"] = feed["url"].replace("party", "su")
-
-    # Raindrop
-    # Load env
-    load_dotenv()
-    access_token = os.getenv("RAINDROPIO_ACCESS_TOKEN")
+def get_from_raindrop(collection_id):
     url = "https://api.raindrop.io/rest/v1"
     endpoint = "/raindrops"
     headers = {
@@ -31,39 +20,98 @@ if __name__ == "__main__":
         "perpage": 50,
     }
 
-    unmarked = 36039260  # collection_id
-    marked = 36039262  # collection_id
-
     resp = requests.get(
-        f"{url}{endpoint}/{unmarked}",
+        f"{url}{endpoint}/{collection_id}",
         headers=headers,
         params=query,
     )
+
+    if resp.status_code != requests.codes.ok:
+        print(resp.text)
+        exit()
+
     time.sleep(1)
+    return resp
+
+
+def move_marked_raindrop(collection_id, items, marked):
+    url = "https://api.raindrop.io/rest/v1"
+    endpoint = "/raindrops"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {access_token}"
+    }
+    query = {
+        # "sort": "domain",
+        # "page": 1,
+        "perpage": 50,
+        # "dengerAll": True,
+    }
+    body = {
+        "ids": items,
+        "collectionId": marked,
+    }
+
+    resp = requests.put(
+        f"{url}{endpoint}/{unmark}",
+        headers=headers,
+        params=query,
+        json=body,
+    )
 
     if resp.status_code != requests.codes.ok:
         print(resp.text)
         exit(1)
 
+    time.sleep(1)
+    return resp
+
+
+def unify_userdata(username):
+    # name to id
+    kakasi = pykakasi.kakasi()
+    kakasi.setMode("H", "a")
+    kakasi.setMode("K", "a")
+    kakasi.setMode("J", "a")
+    conv = kakasi.getConverter()
+
+    username = conv.do(username)
+    pattern = r"[^a-zA-Z0-9]"
+    username = re.sub(pattern, "_", username)
+    return username
+
+
+if __name__ == "__main__":
+    # Raindrop
+    # Load env
+    load_dotenv()
+
+    with open("weneedfeed.yml", "r") as f:
+        feeds = yaml.safe_load(f)
+
+    # Unify domains
+    for feed in feeds["pages"]:
+        feed["url"] = feed["url"].replace("party", "su")
+
+    access_token = os.getenv("RAINDROPIO_ACCESS_TOKEN")
+    unmark = int(os.getenv("UNMARK"))  # collection_id
+    marked = int(os.getenv("MARKED"))
+
+    # Get unmarked raindrops
+    resp = get_from_raindrop(unmark)
+    if resp.json()["count"] == 0:
+        print("There is no unmark raindrops")
+        exit()
+
+    raindrops = []
     for item in resp.json()["items"]:
         if "kemono" not in item["domain"]:
             continue
 
-        # name to id
-        kakasi = pykakasi.kakasi()
-        kakasi.setMode("H", "a")
-        kakasi.setMode("K", "a")
-        kakasi.setMode("J", "a")
-        conv = kakasi.getConverter()
-
-        username = item["title"].split(" ")[2]
-        username = conv.do(username)
-        pattern = r"[^a-zA-Z0-9]"
-        username = re.sub(pattern, "_", username)
-
         parsed = urlparse(item["link"])
         service = parsed.path.split("/")[1]
         _id = parsed.path.split("/")[3]
+        username = unify_userdata(item["title"].split(" ")[2])
 
         page = {
             'id': f"{username}",
@@ -76,6 +124,9 @@ if __name__ == "__main__":
             'url': f"https://kemono.su/{service}/user/{_id}",
         }
         feeds["pages"].append(page)
+        raindrops.append(item["_id"])
+
+    r = move_marked_raindrop(unmark, raindrops, marked)
 
     # sort pages
     pages = sorted(feeds["pages"], key=lambda x: x["id"])
